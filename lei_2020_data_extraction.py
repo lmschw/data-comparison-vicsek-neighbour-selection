a7022b9c2cb1aeee1973aa9780b9ad8b0b8562f3
lmschw <lillyschwarzenbach@gmail.com>
2024-08-07 07:24:02 +0200

    update data extraction process Lei et al. (2020) - store both original and translated x, y, u, v

--- lei_2020_data_extraction.py
+++ lei_2020_data_extraction.py
@@ -44,21 +44,35 @@ def angleBetweenTwoVectors(vec1, vec2):
     """
     return np.arctan2(vec1[1]-vec2[1], vec1[0]-vec2[0])
 
-def computeOrientation(row):
-    nextPositionDf = dfExp.loc[(dfExp['time'] == row['time'] +1) & (dfExp['exp_id'] == row['exp_id']) & (dfExp['fish_id'] == row['fish_id']) , ['x', 'y']]
+def computeOrientation(row, translated=False):
+    if translated == True:
+        x = 'x'
+        y = 'y'
+    else:
+        x = 'x_translated'
+        y = 'y_translated'
+    nextPositionDf = dfExp.loc[(dfExp['timestep'] == row['timestep'] +1) & (dfExp['exp_id'] == row['exp_id']) & (dfExp['fish_id'] == row['fish_id']) , [x, y]]
     if nextPositionDf.shape[0] == 1:
-        vec1 = [nextPositionDf['x'].iloc(0)[0], nextPositionDf['x'].iloc(0)[0]]
-        vec0 = [row['x'], row['y']]
+        vec1 = [nextPositionDf[x].iloc(0)[0], nextPositionDf[x].iloc(0)[0]]
+        vec0 = [row[x], row[x]]
         return computeUvCoordinates(angleBetweenTwoVectors(vec0, vec1))
     return [0,0]
 
 def getOrientationSeries(row):
-    orient = computeOrientation(row)
+    orient = computeOrientation(row, translated=False)
+    return pd.Series({'u': orient[0], 'v': orient[1]})
+
+def getOrientationSeriesTranslated(row):
+    orient = computeOrientation(row, translated=True)
     return pd.Series({'u': orient[0], 'v': orient[1]})
 
 def getTimesPositionsOrientationsColoursFromDf(df):
+    x = 'x_translated'
+    y = 'y_translated'
+    u = 'u_translated'
+    v = 'v_translated'
     n = int(np.max(df['fish_id']))
-    tmax = np.max(df['time'])
+    tmax = np.max(df['timestep'])
 
     times = []
     positions =[]
@@ -72,14 +86,14 @@ def getTimesPositionsOrientationsColoursFromDf(df):
         orientationsT = []
         coloursT = []
 
-        dfT = df.loc[(df['time'] == t), ['exp_id','fish_id', 'time', 'x', 'y', 'u', 'v']]
+        dfT = df.loc[(df['timestep'] == t), ['exp_id','fish_id', 'timestep', x, y, u, v]]
         for i in range(1, n+1):
             coloursT.append("k")
 
-            dfI = dfT.loc[(df['fish_id'] == i), ['exp_id','fish_id', 'time', 'x', 'y', 'u', 'v']]
+            dfI = dfT.loc[(df['fish_id'] == i), ['exp_id','fish_id', 'timestep', x, y, u, v]]
             if dfI.shape[0] == 1:
-                positionsT.append([dfI['x'].iloc(0)[0], dfI['y'].iloc(0)[0]])
-                orientationsT.append([dfI['u'].iloc(0)[0], dfI['v'].iloc(0)[0]]) 
+                positionsT.append([dfI[x].iloc(0)[0], dfI[y].iloc(0)[0]])
+                orientationsT.append([dfI[u].iloc(0)[0], dfI[v].iloc(0)[0]]) 
             else:
                 positionsT.append([-100, -100])
                 orientationsT.append([0,0])
@@ -104,45 +118,58 @@ factor = 1
 filename = "data/Lei_2020/lei_2020_fish_data.csv"
 df = pd.read_csv(filename)
 
-df['time'] = 0
+df['timestep'] = 0
+df['x_translated'] = 0.0
+df['y_translated'] = 0.0
 df['u'] = 0.0
 df['v'] = 0.0
+df['u_translated'] = 0.0
+df['v_translated'] = 0.0
+
 
 print(np.max(df['exp_id']))
-#for expId in df['exp_id'].unique():
+for expId in df['exp_id'].unique():
 #for expId in [1]:
-for expId in range(1311, np.max(df['exp_id'])):
+#for expId in range(2, np.max(df['exp_id'])):
+    filename = f"lei_2020_extracted_data/lei_2020_expId={expId}"
+    
     dfExp = df.loc[(df['exp_id'] == expId), ['exp_id', 'fish_id', 't', 'x', 'y']]
 
-    dfExp["time"] = dfExp['t'].rank(method='dense').astype(int)
-    dfExp["time"] = dfExp['time'] -1 # starting from 0
+    dfExp["timestep"] = dfExp['t'].rank(method='dense').astype(int)
+    dfExp["timestep"] = dfExp['timestep'] -1 # starting from 0
 
-    dfExp['time'] = dfExp['time'].astype(int)
+    dfExp['timestep'] = dfExp['timestep'].astype(int)
     dfExp['exp_id'] = dfExp['exp_id'].astype(int)
     dfExp['fish_id'] = dfExp['fish_id'].astype(int)
 
     #dfcur = dfExp
     #print(np.min(dfcur['x']), np.max(dfcur['x']), np.min(dfcur['y']), np.max(dfcur['y']))
-    dfExp['x'] = (dfExp['x'] + 1) * factor
-    dfExp['y'] = (dfExp['y'] + 1) * factor
+    dfExp['x_translated'] = (dfExp['x'] + 1) * factor
+    dfExp['y_translated'] = (dfExp['y'] + 1) * factor
 
     dfOrient = dfExp.apply(getOrientationSeries, axis=1)
+    dfOrientTranslated = dfExp.apply(getOrientationSeriesTranslated, axis=1)
     dfExp['u'] = dfOrient['u']
     dfExp['v'] = dfOrient['v']
+    dfExp['u_translated'] = dfOrientTranslated['u']
+    dfExp['v_translated'] = dfOrientTranslated['v']
 
-    
+    print(dfExp['u'] == dfExp['u_translated'])
+
     #print(dfExp.head())
     df[df['exp_id'] == expId] = dfExp
     #print(df.head())
 
-    tmax = np.max(dfExp['time'])
+    df.to_csv(f"{filename}.csv", index=False)
+
+    tmax = np.max(dfExp['timestep'])
 
     # transforming the df into positions and orientations
     times, positions, orientations, colours = getTimesPositionsOrientationsColoursFromDf(dfExp)
 
     simulationData = (times, np.array(positions), np.array(orientations))
 
-    filename = f"lei_2020_extracted_data/lei_2020_expId={expId}"
+    
 
     ServiceSavedModel.saveModel(simulationData=simulationData, colours=colours, path=f"{filename}.json")
 
